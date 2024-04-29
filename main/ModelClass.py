c601c6cdeed50713ca347e94e1ae90096c13c49d
nkhan364 <naveedkhan13041998@gmail.com>
2024-04-29 11:18:16 -0400

    final updates

--- main/ModelClass.py
+++ main/ModelClass.py
@@ -1,11 +1,13 @@
 import os
 import json
 from django.conf import settings
-from tensorflow.keras import layers, models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.applications import VGG16
 
 
 class ModelClass:
-    def __init__(self, model_name="CustomCNN"):
+    def __init__(self, model_name="FERPLUS"):
         lookup_path = os.path.join(
             settings.BASE_DIR, f"main/data/{model_name}/data_lookup_train.json"
         )
@@ -23,42 +25,55 @@ class ModelClass:
             lookup = json.load(json_file)
         return {str(value): key.upper() for key, value in lookup.items()}
 
-    def load_custom_cnn_model(self, weights_file):
-        self.IMAGE_RES = 100
-        model = models.Sequential(
-            [
-                layers.Conv2D(
-                    32,
-                    (3, 3),
-                    activation="relu",
-                    input_shape=(self.IMAGE_RES, self.IMAGE_RES, 3),
-                ),
-                layers.MaxPooling2D((2, 2)),
-                layers.Conv2D(64, (3, 3), activation="relu"),
-                layers.MaxPooling2D((2, 2)),
-                layers.Conv2D(64, (3, 3), activation="relu"),
-                layers.Flatten(),
-                layers.Dense(64, activation="relu"),
-                layers.Dense(self.num_classes, activation="softmax"),
-            ]
+    def load_ferplus(self, weights_file):
+        # self.IMAGE_RES = 100
+        model = Sequential()
+        model.add(
+            Conv2D(
+                32,
+                (3, 3),
+                activation="relu",
+                input_shape=(self.IMAGE_RES, self.IMAGE_RES, 3),
+            )
         )
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(64, activation="relu"))
+        model.add(Dense(self.num_classes, activation="softmax"))
 
         model.load_weights(weights_file)
 
         return model
 
-    def load_resnet50_model(self, weights_file): ...
+    def load_resnet50_model(self, weights_file):
+
+        base_model = VGG16(
+            weights="imagenet",
+            include_top=False,
+            input_shape=(self.IMAGE_RES, self.IMAGE_RES, 3),
+        )
+        for layer in base_model.layers:
+            layer.trainable = False
+        model = Sequential()
+        model.add(base_model)
+        model.add(Flatten())
+        model.add(Dense(64, activation="relu"))
+        model.add(Dense(self.num_classes, activation="softmax"))
+        model.load_weights(weights_file)
+        return model
 
     def load_model(self, model_name, weights_file):
-        if model_name == "CustomCNN":
+        if model_name == "FERPLUS":
             self.IMAGE_RES = 100
-            return self.load_custom_cnn_model(weights_file)
-        elif model_name == "ResNet50":
+            return self.load_ferplus(weights_file)
+        elif model_name == "VGG16":
             return self.load_resnet50_model(weights_file)
         # Add more models here as needed
         else:
             raise ValueError(
-                "Invalid model name. Supported models are 'CustomCNN' and 'ResNet50'."
+                "Invalid model name. Supported models are 'FERPLUS' and 'VGG16'."
             )
 
         return None
